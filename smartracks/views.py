a21a155b49105118f34754ff386cd0dc31a906ae
Badar <badarwaqar510@gmail.com>
2022-06-20 00:08:26 +0500

    manual update backend

--- smartracks/views.py
+++ smartracks/views.py
@@ -16,6 +16,23 @@ def smartracks(request):
 
 @login_required
 def warehouse(request, pk):
+    if request.method == "POST" and request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest":
+        form_data = QueryDict(request.POST["serializedForm"].encode("ASCII"))
+        url = "https://api.airliftgrocer.com/compartment/status"
+
+        payload = json.dumps(
+            {"warehouseId": warehouse.warehouseId,
+                "rack": form_data["compartment"], "status": form_data["status"], "side": form_data["side"]}
+        )
+        headers = {"auth": "Groc3R@Sm@rtR@ck",
+                   "Content-Type": "application/json"}
+
+        response = requests.request("PUT", url, headers=headers, data=payload)
+
+        if response.status_code == 200:
+            return JsonResponse({"message": "Updated Successfully"})
+        else:
+            return JsonResponse({"message": "Request Failed"})
     allCompartments = Compartment.objects.filter(warehouse_id=pk)
     context = {"allCompartments": allCompartments}
     return render(request, "smartracks/warehouse.html", context)
